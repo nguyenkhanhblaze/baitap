b298007ad815ad32e9e5f3dbb44e69fb1f25e483
nguyenkhanhblaze <nguyenkhanhblaze@gmail.com>
2016-06-03 15:54:42 +0700

    database
    database

---
+++ test/database/app/src/main/java/com/example/latitude/database/Country.java
@@ -0,0 +1,64 @@
+package com.example.latitude.database;
+
+/**
+ * Created by Latitude on 6/3/2016.
+ */
+public class Country {
+    public final static String TABLE_NAME = "Country";
+    public final static String COL_ID = "_id";
+    public final static String COL_NAME_EN = "nameEn";
+    public final static String COL_NAME_VI = "nameVi";
+    public final static String COL_FLAG = "flag";
+    public final static String CREATE_TABLE = "create table if not exists " +TABLE_NAME+"("
+            + COL_ID + " integer primary key, "
+            + COL_NAME_EN  + " text,"
+            + COL_NAME_VI + " text,"
+            + COL_FLAG + " text)";
+    public final static String DROP_TABLE = "drop table if exists " + TABLE_NAME;
+    int _id;
+    String nameEn;
+    String nameVi;
+    String flag;
+
+    public Country() {
+    }
+
+    public Country(int _id, String nameEn, String nameVi, String flag) {
+        this._id = _id;
+        this.nameEn = nameEn;
+        this.nameVi = nameVi;
+        this.flag = flag;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public void setNameEn(String nameEn) {
+        this.nameEn = nameEn;
+    }
+
+    public String getNameVi() {
+        return nameVi;
+    }
+
+    public void setNameVi(String nameVi) {
+        this.nameVi = nameVi;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+}
---
+++ test/database/app/src/main/java/com/example/latitude/database/CountryAdapter.java
@@ -0,0 +1,63 @@
+package com.example.latitude.database;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryAdapter extends ArrayAdapter<Country> {
+
+    Context mContext;
+    int mResource;
+    List<Country> mCountries;
+    public CountryAdapter(Context context, int resource,
+                          List<Country> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+        mCountries = objects;
+}
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        ViewHolder viewHolder = null;
+        if(convertView == null){
+            //
+            LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //
+            convertView = layoutInflater.inflate(mResource, parent, false);
+
+            //
+            viewHolder = new ViewHolder();
+            //
+            viewHolder.tvNameEn = (TextView) convertView.findViewById(R.id.tvNameEn);
+            viewHolder.tvNameVi = (TextView) convertView.findViewById(R.id.tvNameVi);
+            viewHolder.ivFlag = (ImageView) convertView.findViewById(R.id.ivFlag);
+
+            //
+            convertView.setTag(viewHolder);
+        }else
+            viewHolder = (ViewHolder) convertView.getTag();
+
+        //
+        viewHolder.tvNameEn.setText(mCountries.get(position).getNameEn());
+        viewHolder.tvNameVi.setText(mCountries.get(position).getNameVi());
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        TextView tvNameEn;
+        TextView tvNameVi;
+        ImageView ivFlag;
+    }
+    }
