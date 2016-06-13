09e6544c27b600f4fb43de1948380b6be2dc5892
nguyenkhanhblaze <nguyenkhanhblaze@gmail.com>
2016-06-13 15:57:06 +0700

    menu
    menu

---
+++ test/Menu/app/src/main/java/com/example/latitude/menu/DemoOptionMenu.java
@@ -0,0 +1,38 @@
+package com.example.latitude.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class DemoOptionMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_option_menu);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.demo_option_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.mnInsert){
+            Toast.makeText(DemoOptionMenu.this, "Insert", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(id == R.id.mnRefesh){
+            Toast.makeText(DemoOptionMenu.this, "Refesh", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(id == R.id.mnSearch){
+            Toast.makeText(DemoOptionMenu.this, "Search", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
